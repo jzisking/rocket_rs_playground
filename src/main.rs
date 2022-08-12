77069272d2b150647acb9b81162511a54ea19e78
jzisking <103643624+jzisking@users.noreply.github.com>
2022-08-12 16:47:42 -0700

    Some new features tested

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::borrow::{Borrow, Cow};
+use std::ops::Add;
 use std::path::PathBuf;
 use anyhow::Result;
 use rocket::fs::{FileServer, Options};
+use rocket::http::RawStr;
 use rocket::Request;
+use rocket::request::{FromRequest, Outcome};
 
 #[macro_use] extern crate rocket;
 
@@ -44,10 +48,36 @@ fn combine(test: PathBuf) -> String {
     test.into_os_string().to_str().unwrap().to_owned()
 }
 
+#[get("/a/b/<_>/d/<e>")]
+fn a(e: i32) -> String {
+    format!("{}", e + 1)
+}
+
+#[get("/h/i/<_..>/z/<a>")]
+fn b(a: i32) -> String {
+    format!("{}", a + 2)
+}
+
+#[get("/t1/<param>")]
+fn t1_1(param: bool) -> String {
+    format!("{}", if param { "This is true value"} else {"This is false value"})
+}
+
+#[get("/t1/<param>", rank = 2)]
+fn t1_2(param: &str) -> String {
+    format!("We have a string now {}", param)
+}
+
+#[get("/query/hello?wave&<name>")]
+fn query(name: Cow<str>) -> String {
+    format!("Hello: {}", name)
+}
+
 #[rocket::main]
 async fn main() {
     let ignite = rocket::build()
-        .mount("/", routes![index, api, test, another_test, file, combine])
+        .mount("/", routes![index, api, test, another_test, file, combine, a, b,
+            t1_1, t1_2, query])
         .mount("/static/", FileServer::new("static/", Options::default()))
         .register("/", catchers![catcher]).launch().await.unwrap();
 }
